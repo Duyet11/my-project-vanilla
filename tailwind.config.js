bc88f667a26cf5082a9c66cc187a8a74dc004285
Duyet11 <duyetptph29382@fpt.edu.vn>
2024-01-07 13:35:43 +0700

    update news

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/components/*.{html,js}",
+    "./src/pages/*.{html,js}",
+    "./index.html"
+  ],
+  prefix: "tw-",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
